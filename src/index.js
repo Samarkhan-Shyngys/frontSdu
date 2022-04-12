271473162cce5424eae4da7c95d76923711a2ffa
Akhjol <akhjoltaukye@gmail.com>
2022-04-12 22:09:00 +0800

    2022/04/12

--- src/index.js
+++ src/index.js
@@ -3,19 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
-import LibraryService from './topics/LibraryService';
 
 export const Context = createContext(null)
 
 ReactDOM.render(
 
   <BrowserRouter>
-  <Context.Provider value={{
-        listt: new LibraryService(),
-        
-    }}>
-      <App />
-    </Context.Provider>
+  
+      <App />,
     
     </BrowserRouter>,
   document.getElementById('root')
