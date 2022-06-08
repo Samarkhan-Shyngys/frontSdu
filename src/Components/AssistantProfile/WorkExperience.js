39e7474370d8d2f877526dad9b208dcb3bdde1a2
Shyngys <chingisss01@gmail.com>
2022-06-08 17:51:18 +0600

    add git work

--- src/Components/AssistantProfile/WorkExperience.js
+++ src/Components/AssistantProfile/WorkExperience.js
@@ -17,140 +17,147 @@ const data = false;
 const WorkExperience = () => {
   const user = JSON.parse(localStorage.getItem("user"));
   const id = user.id;
-
+  const [isFormReady, setIsFormReady] = useState(false)
   const [theArray, setTheArray] = useState([]);
-  
-  const [theInitials, setTheInitials] = useState();
-  useEffect(()=>{
-    axios.get( `${base_url}/api/assistant/get/work/${id}`)
-    .then((result) => setTheArray(result.data.jobs))
-    .catch(((er)=>console.log(er)))
-  }, []);
   const initialValues = {
     job: 
-    theArray
+    [
+      {
+        organisation: "ssss",
+        position: "",
+        startWorkYear: "",
+        startWorkMonth: "",
+        endDate: "",
+        endWorkMonth: "",
+        endWorkYear: "",
+      }
+    ] 
   };
+  const [theInitials, setTheInitials] = useState(initialValues);
+  useEffect(()=>{
+    axios.get( `${base_url}/api/assistant/get/work/${id}`)
+    .then((result) => setTheInitials({job:  result.data.jobs}), setIsFormReady(true))
+    .catch(((er)=>console.log(er)))
+  }, []);
   // setTheWorks(theArray);
-  console.log('ls: ' + theArray); 
-  console.log('ls2: ' ); 
-  console.log('lss: ' + JSON.stringify(initialValues.job[0]))
+  console.log('ls: ' + JSON.stringify(theInitials.job)); 
+  console.log('isReady: ' + isFormReady)
+  // console.log('ls2: ' + JSON.stringify(theInitials.job[0])); 
+  // console.log('lss: ' + JSON.stringify(initialValues.job[0]))
   return (
     <Box fullWidth>
-      <Formik
-       initialValues={initialValues}
-       onSubmit={values =>console.log(values)}
-       >
-      {({ values }) => (
-
-        <Form>
-            <FieldArray name="job">
-              {({ remove, push, control }) => (
-                <Grid container spacing={3} component="section">
-                  {
-                    values.job.map((job, index) => (
-                      <React.Fragment key={`${index}${job.organisation}`}>
-                        <Grid item xs={12}>
-                          <Field
-                            name={`job.${index}.organisation`}
-                            control={control}
-                            label="Организация"
-                            component={TextField}
-                            fullWidth
-                          />
-                          <Field
-                        name="firstName"
-                        label="Имя"
-                        fullWidth
-                        component={TextField}
-                        value={job.organisation}
-                        placeholder={"Введите ваше имя"}
-                        
-                      />
-                        </Grid>
-                        <Grid item xs={12}>
-                          <Field
-                            name={`job.${index}.position`}
-                            label="Должность"
-                            component={TextField}
-                            fullWidth
-                          />
-                        </Grid>
-                        <Grid item xs={12}>
-                        <Typography variant='subtitle1'>Начало работы</Typography>
-                        </Grid>
-                        <Grid item xs={9}>
-                          <SelectField
-                            name={`job.${index}.startWorkMonth`}
-                            label="Месяц"
-                            data={Months}
-                            fullWidth
-                          />
-                        </Grid>
+      {
+        isFormReady? <Formik
+        initialValues={theInitials}
+        onSubmit={values =>console.log(values)}
+        >
+       {({ values }) => (
+       
+          
+         <Form>
+             <FieldArray name="job">
+               {({ remove, push, control }) => (
+                 <Grid container spacing={3} component="section">
+                   {values.job.length>0 &&
+                     values.job.map((job, index) => (
+                       <React.Fragment key={`${index}${job.organisation}`}>
+                         <Grid item xs={12}>
+                           <Field
+                             name={`job.${index}.organisation`}
+                             value={job.organisation}
+                             control={control}
+                             label="Организация"
+                             component={TextField}
+                             fullWidth
+                           />
+                           
+                         </Grid>
+                         <Grid item xs={12}>
+                           <Field
+                             name={`job.${index}.position`}
+                             label="Должность"
+                             component={TextField}
+                             fullWidth
+                           />
+                         </Grid>
+                         <Grid item xs={12}>
+                         <Typography variant='subtitle1'>Начало работы</Typography>
+                         </Grid>
+                         <Grid item xs={9}>
+                           <SelectField
+                             name={`job.${index}.startWorkMonth`}
+                             label="Месяц"
+                             data={Months}
+                             fullWidth
+                           />
+                         </Grid>
+       
+                         <Grid item xs={3}>
+                           <SelectField
+                             name={`job.${index}.startWorkYear`}
+                             label="Год"
+                             data={Years}
+                             fullWidth
+                           />
+                         </Grid>
+                         <Grid item xs={12}>
+                         <Typography variant='subtitle1'>Окончание</Typography>
+                         </Grid>
+                         <Grid item xs={9}>
+                           <SelectField
+                             name={`job.${index}.endWorkMonth`}
+                             label="Месяц"
+                             data={Months}
+                             fullWidth
+                           />
+                         </Grid>
+                         <Grid item xs={3}>
+                           <SelectField
+                             name={`job.${index}.endWorkYear`}
+                             label="Год"
+                             data={Years}
+                             fullWidth
+                           />
+                         </Grid>
+                         <Grid item xs={12}>
+                       <button
+                         type="button"
+                         className="flex space-x-2"
+                         onClick={() => remove(index)}
+                       >
+                         <MinusIcon className="h-6 w-6 border rounded-full mr-2 border-gray-800" />
+                         <span>Удалить место работы </span>
+                       </button>
+                     </Grid>
+                       </React.Fragment>
+                     ))}
+                   <Grid item xs={12}>
+                       <button
+                         type="button"
+                         className="flex space-x-2"
+                         onClick={() => push({organisation: "",
+                           position: "",
+                           startWorkYear: "",
+                           startWorkMonth: "",
+                           endDate: "",
+                           endWorkMonth: "",
+                           endWorkYear: "", })}
+                       >
+                         <PlusIcon className="h-6 w-6 border rounded-full mr-2 border-gray-800" />
+                         <span>Добавить еще место работы </span>
+                       </button>
+                     </Grid>
+                 <Grid item xs={12}>
+                    <Button text="Сохранить изменения" />
+                 </Grid>
+                 </Grid>
+               )}
+             </FieldArray>
+             </Form>
+       )}
+        </Formik>: null
+      }
       
-                        <Grid item xs={3}>
-                          <SelectField
-                            name={`job.${index}.startWorkYear`}
-                            label="Год"
-                            data={Years}
-                            fullWidth
-                          />
-                        </Grid>
-                        <Grid item xs={12}>
-                        <Typography variant='subtitle1'>Окончание</Typography>
-                        </Grid>
-                        <Grid item xs={9}>
-                          <SelectField
-                            name={`job.${index}.endWorkMonth`}
-                            label="Месяц"
-                            data={Months}
-                            fullWidth
-                          />
-                        </Grid>
-                        <Grid item xs={3}>
-                          <SelectField
-                            name={`job.${index}.endWorkYear`}
-                            label="Год"
-                            data={Years}
-                            fullWidth
-                          />
-                        </Grid>
-                        <Grid item xs={12}>
-                      <button
-                        type="button"
-                        className="flex space-x-2"
-                        onClick={() => remove(index)}
-                      >
-                        <MinusIcon className="h-6 w-6 border rounded-full mr-2 border-gray-800" />
-                        <span>Удалить место работы </span>
-                      </button>
-                    </Grid>
-                      </React.Fragment>
-                    ))}
-                  <Grid item xs={12}>
-                      <button
-                        type="button"
-                        className="flex space-x-2"
-                        onClick={() => push({organisation: "",
-                          position: "",
-                          startWorkYear: "",
-                          startWorkMonth: "",
-                          endDate: "",
-                          endWorkMonth: "",
-                          endWorkYear: "", })}
-                      >
-                        <PlusIcon className="h-6 w-6 border rounded-full mr-2 border-gray-800" />
-                        <span>Добавить еще место работы </span>
-                      </button>
-                    </Grid>
-                <Grid item xs={12}>
-                   <Button text="Сохранить изменения" />
-                </Grid>
-                </Grid>
-              )}
-            </FieldArray>
-            </Form>
-      )}
-       </Formik>
     </Box>
   );
 };
